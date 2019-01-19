ffa12d96a80ab841ed06d7a34d82e3620c74760c
Anshil Gandhi <gandhi21299@gmail.com>
2019-01-19 15:29:04 -0700

    git commit

--- force-layout.py
+++ force-layout.py
@@ -1,41 +1,112 @@
 import random
 from tkinter import *
+from tkinter.dnd import Tester as DragWindow, Icon as Dragable
 
-n, m = map(int, input().split())
-dim = 500
+# GLOBAL variables --------------------------------------------------
+WIN_WIDTH = 500
+WIN_HEIGHT = 500
 
-g = [[] for _ in range(n)]
-coor = [(random.randint(0, dim), random.randint(0, dim)) for _ in range(n)]
-edges = []
+# -------------------------------------------------------------------
 
-def clicked():
-    print("clicked registered")
+class DragManager():
+    def add_dragable(self, widget):
+        widget.bind("<ButtonPress-1>", self.on_start)
+        widget.bind("<B1-Motion>", self.on_drag)
+        widget.bind("<ButtonRelease-1>", self.on_drop)
+        widget.configure(cursor="hand1")
 
-for _ in range(m):
-    u, v = map(int, input().split())
-    edges.append((u,v))
-    g[u].append(v)
-    g[v].append(u)
+    def on_start(self, event):
+        # you could use this method to create a floating window
+        # that represents what is being dragged.
+        pass
 
-master = Tk()
-master.title("Force-based Graph Layout")
+    def on_drag(self, event):
+        # you could use this method to move a floating window that
+        # represents what you're dragging
+        pass
 
-canvas = Canvas(master, width=500, height=500)
-canvas.pack()
+    def on_drop(self, event):
+        # find the widget under the cursor
+        x,y = event.widget.winfo_pointerxy()
+        target = event.widget.winfo_containing(x,y)
+        try:
+            target.configure(image=event.widget.cget("image"))
+        except:
+            pass
 
-nodes = [Button(canvas, text=str(i), command = clicked) for i in range(n)]
-for i in range(n):
-    canvas.create_window(*coor[i], window=nodes[i]) 
 
-print(coor)
-for e in edges:
-    print(e)
-    canvas.create_line(*coor[e[0]], *coor[e[1]])
+# Window class ------------------------------------------------------
+class Window:
+	def __init__(self, winTitle, winWidth, winHeight):
+		self.master = Tk()
+		self.master.title(winTitle)
 
-def close_window():
-    master.destroy()
+		self.canvas = Canvas(
+						self.master, 
+						width=winWidth, 
+						height=winHeight
+					)
+		self.canvas.pack()
+		self.nodes = list()
+		self.edges = list()
+		self.coor = list()
 
-exit_btn = Button(master, text = "Close", command = close_window)
-exit_btn.pack()
+		self.master.withdraw()
 
-canvas.mainloop()
+		self.dragWindow = DragWindow(self.master)
+
+	def add_node(self, lbl):
+		self.nodes.append(Button(self.dragWindow.top, text=lbl, command=self.make_btn, args=lbl).pack())
+
+	def make_btn(self,lbl):
+		Dragable(lbl).attach(self.dragWindow.canvas)
+
+	def add_edge(self, edge):
+		self.edges.append(edge)
+
+	def init_coords(self):	
+		self.coor = [(	random.randint(0, WIN_WIDTH), 
+			random.randint(0, WIN_HEIGHT)) 
+			for _ in range(len(self.nodes))]
+
+	def render(self):
+		for i in range(len(self.nodes)):
+			self.canvas.create_window(*self.coor[i], window=self.nodes[i])
+		print(self.coor)
+
+		for edge in self.edges:
+			print(edge)
+			self.canvas.create_line(*self.coor[edge[0]], 
+				*self.coor[edge[1]])
+
+	def close(self, ):
+		self.master.destroy()
+
+# -------------------------------------------------------------------
+
+# Procedures --------------------------------------------------------
+def input_graph():
+	n, m = map(int, input().split())
+	g = [[] for i in range(n)]	# adjacency list rep of a graph
+
+	for i in range(n):
+		win.add_node(str(i))
+
+	edges = []	# edge list
+	for i in range(m):
+		u, v = map(int, input().split())
+		edges.append((u,v))
+		g[u].append(v)
+		g[v].append(u)
+		win.add_edge((u,v))
+# -------------------------------------------------------------------
+
+if __name__ == '__main__':
+	win = Window("Force-based layout", 500, 500)
+	input_graph()
+	win.init_coords()
+	print("rendering graph")
+	win.render()
+	win.canvas.mainloop()
+
+

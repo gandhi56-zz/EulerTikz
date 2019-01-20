fc0be19a1ee959ecd2f12db0bf0578aabc624723
Anshil Gandhi <gandhi21299@gmail.com>
2019-01-19 18:07:22 -0700

    git commit

--- force-layout.py
+++ force-layout.py
@@ -5,96 +5,80 @@ from tkinter.dnd import Tester as DragWindow, Icon as Dragable
 # GLOBAL variables --------------------------------------------------
 WIN_WIDTH = 500
 WIN_HEIGHT = 500
-
+SIZE = 20
+NUM_NODES = 100
+NUM_EDGES = 100
 # -------------------------------------------------------------------
 
-class DragManager():
-    def add_dragable(self, widget):
-        widget.bind("<ButtonPress-1>", self.on_start)
-        widget.bind("<B1-Motion>", self.on_drag)
-        widget.bind("<ButtonRelease-1>", self.on_drop)
-        widget.configure(cursor="hand1")
-
-    def on_start(self, event):
-        # you could use this method to create a floating window
-        # that represents what is being dragged.
-        pass
-
-    def on_drag(self, event):
-        # you could use this method to move a floating window that
-        # represents what you're dragging
-        pass
-
-    def on_drop(self, event):
-        # find the widget under the cursor
-        x,y = event.widget.winfo_pointerxy()
-        target = event.widget.winfo_containing(x,y)
-        try:
-            target.configure(image=event.widget.cget("image"))
-        except:
-            pass
-
-
 # Window class ------------------------------------------------------
-class Window:
+class Window(Tk):
 	def __init__(self, winTitle, winWidth, winHeight):
-		self.master = Tk()
-		self.master.title(winTitle)
-
-		self.canvas = Canvas(
-						self.master, 
-						width=winWidth, 
-						height=winHeight
-					)
+		super().__init__()
+		self.title(winTitle)
+		self.canvas = Canvas(self.master, width=winWidth, height=winHeight,bg="white")
 		self.canvas.pack()
 		self.nodes = list()
 		self.edges = list()
-		self.coor = list()
-
-		self.master.withdraw()
-
-		self.dragWindow = DragWindow(self.master)
+		self.coor = {}
+		for i in range(NUM_NODES):
+			self.coor[str(i)] = (random.randint(0, WIN_WIDTH), 
+								random.randint(0, WIN_HEIGHT))
+		self.dnd = None
+		self.lines = list()
+		self.adjList = {}
 
 	def add_node(self, lbl):
-		self.nodes.append(Button(self.dragWindow.top, text=lbl, command=self.make_btn, args=lbl).pack())
+		# self.nodes.append(Button(self.dragWindow.top, text=lbl, command=self.make_btn, args=lbl).pack())
+		
+		if lbl not in self.adjList:
+			self.adjList[lbl] = list()
 
-	def make_btn(self,lbl):
-		Dragable(lbl).attach(self.dragWindow.canvas)
+		x = self.coor[lbl][0]
+		y = self.coor[lbl][1]
+		self.canvas.create_oval(x, y, x+SIZE, y+SIZE, fill = "pink", tags="draggable")
+		self.canvas.tag_bind("draggable", "<ButtonPress-1>", self.button_press)
+		self.canvas.tag_bind("draggable", "<Button1-Motion>", self.button_motion)
+		self.canvas.update()
+
+	def button_press(self, event):
+		item = self.canvas.find_withtag(CURRENT)
+		self.dnd = (item, event.x, event.y)
+		print("click:", self.dnd)
+
+	def button_motion(self, event):
+		x, y = event.x, event.y
+		item, x0, y0 = self.dnd
+		self.canvas.move(item, x - x0, y - y0)
+		self.dnd = (item, x, y)
 
 	def add_edge(self, edge):
 		self.edges.append(edge)
-
-	def init_coords(self):	
-		self.coor = [(	random.randint(0, WIN_WIDTH), 
-			random.randint(0, WIN_HEIGHT)) 
-			for _ in range(len(self.nodes))]
+		self.adjList[edge[0]].append(edge[1])
+		self.adjList[edge[1]].append(edge[0])
 
 	def render(self):
 		for i in range(len(self.nodes)):
 			self.canvas.create_window(*self.coor[i], window=self.nodes[i])
-		print(self.coor)
 
 		for edge in self.edges:
-			print(edge)
-			self.canvas.create_line(*self.coor[edge[0]], 
-				*self.coor[edge[1]])
+			self.canvas.create_line(*self.coor[edge[0]], *self.coor[edge[1]])
 
-	def close(self, ):
+	def close(self):
 		self.master.destroy()
 
 # -------------------------------------------------------------------
 
 # Procedures --------------------------------------------------------
 def input_graph():
-	n, m = map(int, input().split())
-	g = [[] for i in range(n)]	# adjacency list rep of a graph
+	NUM_NODES, NUM_EDGES = map(int, input().split())
+	g = {str(i) : [] for i in range(NUM_NODES)}	# adjacency list rep of a graph
 
-	for i in range(n):
+	for i in range(NUM_NODES):
 		win.add_node(str(i))
 
 	edges = []	# edge list
-	for i in range(m):
-		u, v = map(int, input().split())
+	for i in range(NUM_EDGES):
+		u, v = map(str, input().split())
 		edges.append((u,v))
 		g[u].append(v)
 		g[v].append(u)
@@ -104,8 +88,6 @@ def input_graph():
 if __name__ == '__main__':
 	win = Window("Force-based layout", 500, 500)
 	input_graph()
-	win.init_coords()
-	print("rendering graph")
 	win.render()
 	win.canvas.mainloop()
 
--- ovaldnd.py
+++ ovaldnd.py
@@ -4,4 +4,27 @@ class App(tk.Tk):
 	def __init__(self):
 		super().__init__()
 		self.title("Drag and drop")
-		self.
+		self.dnd = None
+		self.canvas = tk.Canvas(self, bg="white")
+		self.canvas.pack()
+
+		x, y = 30,30
+		self.canvas.create_oval(x, y, 60, 60, fill="green", tags="draggable")
+		# self.canvas.create_text(x+15, y+15, text="0")
+		self.canvas.tag_bind("draggable", "<ButtonPress-1>", self.button_press)
+		self.canvas.tag_bind("draggable", "<Button1-Motion>", self.button_motion)
+		self.canvas.update()
+
+	def button_press(self, event):
+		item = self.canvas.find_withtag(tk.CURRENT)
+		self.dnd = (item, event.x, event.y)
+
+	def button_motion(self, event):
+		x, y = event.x, event.y
+		item, x0, y0 = self.dnd
+		self.canvas.move(item, x - x0, y - y0)
+		self.dnd = (item, x, y)
+
+if __name__ == '__main__':
+	app = App()
+	app.mainloop()

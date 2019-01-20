9f537aeaf653e8cf8c364348192e7bd6df2a822a
Anshil Gandhi <gandhi21299@gmail.com>
2019-01-19 18:14:20 -0700

    reorganized files

--- dndbtn.py
+++
@@ -1,9 +0,0 @@
-from tkinter import * #global tkinter import
-from tkinter.dnd import Tester as Dragwindow, Icon as Dragable #import screen and object dragable classes
-root=Tk() #make our root window
-root.withdraw() #hide it since we don't need it
-main=Dragwindow(root) #make our actual main window, it can have dragable objects on
-def make_btn(): #make a new 'B' button
-    Dragable('B').attach(main.canvas) #make it and attach it to our window's functioning part, the canvas
-Button(main.top, text='A', command=make_btn).pack() #make a button 'A' for our window
-mainloop() #start the mainloop
--- tex_scripts/ovaldnd.py
+++
@@ -1,7 +0,0 @@
-import tkinter as tk
-
-class App(tk.Tk):
-	def __init__(self):
-		super().__init__()
-		self.title("Drag and drop")
-		self.
